be57f2516cf677ca8c04e6ccd5d5a5e65103f27a
Hamed Salimian <snbig@pm.me>
2023-10-06 16:05:07 +0330

    Update app.py

--- ASVS_14_5_2/app.py
+++ ASVS_14_5_2/app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/protected', methods=['GET'])
+@app.route('/protected', methods=['GET', 'OPTIONS'])
 def protected_route():
     # Retrieve the 'Origin' header from the request
     origin = request.headers.get('Origin')
 
     # Define a list of allowed origins
-    allowed_origins = ['http://localhost']  # Update with your allowed origins
+    allowed_origins = ['http://localhost', 'https://spotless-frog-wetsuit.cyclic.cloud']  # Update with your allowed origins
 
     # Check if the provided origin is in the allowed origins
     if origin in allowed_origins:
@@ -20,6 +20,10 @@ def protected_route():
         
         # Return the response
         return response
+    elif not origin:
+        response = jsonify({'message': 'no Origin header.'})
+        response.headers['Access-Control-Allow-Origin'] = "null"
+        return response, 403
     else:
         # If the origin is not allowed, return a JSON error response with a 403 status code
         response = jsonify({'error': 'Origin not allowed.'})
