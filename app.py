28f3faa3ac38466e98f06e06d7221d72bae8273f
Hamed Salimian <snbig@pm.me>
2023-12-11 04:17:28 +0000

    Add ASVS 13.3.1 Vulnerable page

--- app.py
+++ app.py
@@ -24,6 +24,40 @@ def delete_account(account_id):
     else:
         return jsonify({'error': 'Account not found'}), 404
 
+# ASVS 13.3.1
+@app.route('/submit-xml', methods=['GET', 'POST'])
+def index():
+    error_message = None
+
+    if request.method == 'POST':
+        xml_data = request.data.decode('utf-8')
+
+        try:
+            # Parse XML data
+            xml_tree = etree.fromstring(xml_data)
+
+            # Validate XML against the XSD schema
+            if schema.validate(xml_tree):
+                # Extract data from the validated XML
+                username = xml_tree.find('.//username').text
+                email = xml_tree.find('.//email').text
+
+                # Process the data (you can perform further processing here)
+                # For demonstration purposes, we just print the data
+                print(f"Username: {username}, Email: {email}")
+                return '', 200  # Successful response
+
+            else:
+                error_message = 'Invalid XML format. Please check your input.'
+
+        except etree.XMLSyntaxError as e:
+            error_message = f'Error parsing XML: {str(e)}'
+
+        return error_message, 400  # Bad request response
+
+    return render_template('ASVS_13_3_1/index.html', error_message=error_message)
+
+
 @app.route('/')
 def redirectToGitPage():
     return redirect("https://snbig.github.io/Vulnerable-Pages/", code=302)
